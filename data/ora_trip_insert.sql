ada51c644e68a85593265e9c0eb522bb90c8ba81
kpiotr6 <piotrek@komlan.pl>
2023-03-21 22:01:40 +0100

    a

--- data/ora_trip_insert.sql
+++ data/ora_trip_insert.sql
@@ -1,26 +1,98 @@
--- trip
-CREATE VIEW Reservations AS
-SELECT COUNTRY_NAME,TRIP_DATE,TRIP_NAME,FIRSTNAME,LASTNAME,r.RESERVATION_ID,l.STATUS
-FROM RESERVATION r
-LEFT JOIN PERSON p ON p.PERSON_ID = r.PERSON_ID
-LEFT JOIN LOG l ON l.RESERVATION_ID = r.RESERVATION_ID
-LEFT JOIN TRIP t ON t.TRIP_ID = r.TRIP_ID
-LEFT JOIN COUNTRIES c ON t.COUNTRY_ID = c.COUNTRIES_ID
+-- drop table log;
+-- drop table reservation;
+-- drop table person;
+-- drop table trip;
+-- drop table countries
+
+--zad1
+create table person
+(
+  person_id int generated always as identity not null,
+  firstname varchar(50),
+  lastname varchar(50),
+  constraint person_pk primary key ( person_id ) enable
+);
+
+create table trip
+(
+  trip_id int generated always as identity not null,
+  trip_name varchar(100),
+  country_id int,
+  trip_date date,
+  max_no_places int,
+  constraint trip_pk primary key ( trip_id ) enable
+);
+
+create table reservation
+(
+  reservation_id int generated always as identity not null,
+  trip_id int,
+  person_id int,
+  status char(1),
+  constraint reservation_pk primary key ( reservation_id ) enable
+);
+
+create table country
+(
+  country_id int generated always as identity not null,
+  country_name varchar(255),
+  constraint country_pk primary key ( country_id ) enable
+);
 
 
-SELECT * FROM Trip;
+alter table reservation
+add constraint reservation_fk1 foreign key
+( person_id ) references person ( person_id ) enable;
 
+alter table reservation
+add constraint reservation_fk2 foreign key
+( trip_id ) references trip ( trip_id ) enable;
+
+alter table reservation
+add constraint reservation_chk1 check
+(status in ('N','P','C')) enable;
+
+alter table trip
+add constraint reservation_fk1 check
+( country_id ) references country ( country_id ) enable;
+
+
+create table log
+(
+	log_id int  generated always as identity not null,
+	reservation_id int not null,
+	log_date date  not null,
+	status char(1),
+	constraint log_pk primary key ( log_id ) enable
+);
+
+alter table log
+add constraint log_chk1 check
+(status in ('N','P','C')) enable;
+
+alter table log
+add constraint log_fk1 foreign key
+( reservation_id ) references reservation ( reservation_id ) enable;
+-- zad2
+-- country
+insert into country(country_name)
+values ('Paryz');
+
+insert into country(country_name)
+values ('Paryz');
+
+--trip
 insert into trip(trip_name, COUNTRY_ID, trip_date, max_no_places)
-values ('Wycieczka do Paryza', 1, to_date('2022-09-12','YYYY-MM-DD'), 3);
+values ('Wycieczka do Paryza', 1, to_date('2022-09-12','YYYY-MM-DD'), 10);
 
 insert into trip(trip_name, COUNTRY_ID, trip_date,  max_no_places)
-values ('Piękny Kraków', 2, to_date('2023-07-03','YYYY-MM-DD'), 2);
+values ('Piękny Kraków', 2, to_date('2023-07-03','YYYY-MM-DD'), 15);
 
 insert into trip(trip_name, COUNTRY_ID, trip_date,  max_no_places)
-values ('Znów do Francji', 1, to_date('2023-05-01','YYYY-MM-DD'), 2);
+values ('Znów do Francji', 1, to_date('2023-05-01','YYYY-MM-DD'), 20);
 
 insert into trip(trip_name, COUNTRY_ID, trip_date,  max_no_places)
-values ('Hel', 2, to_date('2023-05-01','YYYY-MM-DD'), 2);
+values ('Hel', 2, to_date('2023-05-01','YYYY-MM-DD'), 5);
 
 -- person
 
@@ -42,6 +114,21 @@ values  ('Novak', 'Nowak');
 insert into person(firstname, lastname)
 values ('Piotr', 'Piotrowski');
 
+insert into person(firstname, lastname)
+values ('Stanislaw', 'Krupa');
+
+insert into person(firstname, lastname)
+values ('Bernard', 'Skorupa');
+
+insert into person(firstname, lastname)
+values ('Anna', 'Motyl');
+
+insert into person(firstname, lastname)
+values ('Jakub', 'Maslo');
+
+insert into person(firstname, lastname)
+values  ('Katarzyna', 'Ptak');
+
 -- reservation
 -- trip 1
 insert into reservation(trip_id, person_id, status)
@@ -50,15 +137,45 @@ values (1, 1, 'P');
 insert into reservation(trip_id, person_id, status)
 values (1, 2, 'N');
 
+insert into reservation(trip_id, person_id, status)
+values (1, 3, 'P');
+
+insert into reservation(trip_id, person_id, status)
+values (1, 4, 'C');
+
+
 -- trip 2
 insert into reservation(trip_id, person_id, status)
-values (2, 1, 'P');
+values (2, 2, 'P');
 
 insert into reservation(trip_id, person_id, status)
 values (2, 4, 'C');
 
+insert into reservation(trip_id, person_id, status)
+values (2, 5, 'N');
+
+insert into reservation(trip_id, person_id, status)
+values (2, 1, 'C');
+
+
 -- trip 3
 insert into reservation(trip_id, person_id, status)
-values (2, 4, 'P');
+values (3, 6, 'P');
 
-SELECT * FROM PERSON
+insert into reservation(trip_id, person_id, status)
+values (3, 4, 'C');
+
+-- zad3
+CREATE VIEW Reservations AS
+SELECT COUNTRY_NAME,TRIP_DATE,TRIP_NAME,FIRSTNAME,LASTNAME,r.RESERVATION_ID,l.STATUS
+FROM RESERVATION r
+LEFT JOIN PERSON p ON p.PERSON_ID = r.PERSON_ID
+LEFT JOIN LOG l ON l.RESERVATION_ID = r.RESERVATION_ID
+LEFT JOIN TRIP t ON t.TRIP_ID = r.TRIP_ID
+LEFT JOIN COUNTRY c ON t.COUNTRY_ID = c.COUNTRY_ID;
+
+CREATE VIEW Trips AS
+SELECT COUNTRY_NAME,TRIP_DATE,TRIP_NAME,MAX_NO_PLACES,MAX_NO_PLACES - (SELECT COUNT(*) FROM RESERVATION) AS no_available_places
+FROM TRIP t
+LEFT JOIN RESERVATIONS r ON r.TRIP_ID = t.TRIP_ID
+LEFT JOIN COUNTRY c ON c.country_id = t.country_id
